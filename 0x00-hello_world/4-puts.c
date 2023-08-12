8ddcfc8c6d7b340c19b13150bc878cdbc01da424
illa <140983437+3eIlla@users.noreply.github.com>
2023-08-11 19:53:00 -0700

    fix 4/5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  *  main - Entry point
- * 
+ *
  * Description: AC program that prints with put function
- * 
+ *
  * Return: Always 0 (Success)
 */
-
 int main(){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
