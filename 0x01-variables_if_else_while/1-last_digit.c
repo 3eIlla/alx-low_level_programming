936552ec2a1ac5c470dbbc8d52bf315c689e4386
illa <140983437+3eIlla@users.noreply.github.com>
2023-08-14 12:55:07 -0700

    last betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program to print the last digit of the number stored in the variable n.
+ * Description: print last digit of the no. stored in the variable n.
  *
  * Return: Always 0 (Success)
  */
@@ -14,11 +14,11 @@ int n, digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
-    if (digit > 5)
-        printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-    else if (digit == 0)
-        printf("Last digit of %i is %i and is 0\n", n, digit);
-    else 
-        printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+if (digit > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+else if (digit == 0)
+printf("Last digit of %i is %i and is 0\n", n, digit);
+else 
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 return (0);
 }
