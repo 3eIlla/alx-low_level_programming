ee7e3b25fe98dec644e8b1e66287f2874bba5103
illa <140983437+3eIlla@users.noreply.github.com>
2023-08-14 09:42:13 -0700

    Advanced  2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: a C program prints an error massege with write function
+ *
+ * Return: 1 (fail)
+*/
+int main(void)
+{   char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+    write(1, quo, 59);
+return (1);
+}
