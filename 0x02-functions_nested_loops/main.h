6dc1f234453701dca13d83098dcd760311e4bd82
illa <140983437+3eIlla@users.noreply.github.com>
2023-08-15 17:42:49 -0700

    fix man 0.2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,13 +3,13 @@
 
 #include <stdio.h>
 
-//0
+/*0*/
 int _putchar(char c);
-//1
+/*1*/
 void print_alphabet(void);
-//2
+/*2*/
 void print_alphabet_x10(void);
-//3
+/*3*/
 int _islower(int c);
 
 #endif
