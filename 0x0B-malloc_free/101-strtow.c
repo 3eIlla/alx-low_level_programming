e039dc5637752c878686763c69786f2557635f22
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-10-03 04:23:32 -0700

    xb

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -70,7 +70,8 @@ while (str[il] && il == 0)
 		for (ll = 0; ll < jl; ll++)
 			wl[wlc][ll] = str[il + 1];
 		wl[wlc][ll] = '\0';
-		wlc++ && il += 1;
+		wlc += 1;
+		il += 1;
 	}
 	else
 	il = il + 1;
