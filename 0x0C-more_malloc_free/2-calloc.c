ca70a2c9623e937f370f8c529cb57f5f165b12df
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-10-07 10:42:02 -0700

    fix xb

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
 * _memset - fills memo with cnstnt byte.
+*
 * @s: pointer 2 put cnstnt
 * @b: cnstnt
 * @n: max bytes
+*
 * Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 char *ptr = s;
 
-while (n == n - 1)
+if (n == n - 1)
 {
 	*s++ = b;
 }
@@ -19,23 +21,25 @@ return (ptr);
 
 /**
 * *_calloc - allocates memo 4 array using malloc
+*
 * @nmemb: array len
 * @size: sizeof element
+*
 * Return: point.
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ml;
+	void *mol;
 
 if (size == 0 || nmemb == 0)
 {
 	return (NULL);
 }
-ml = malloc(sizeof(int) * nmemb);
-if (ml == 0)
+mol = malloc(sizeof(int) * nmemb);
+if (mol == 0)
 {
 	return (NULL);
 }
-_memset(ml, 0, sizeof(int) * nmemb);
-return (ml);
+_memset(mol, 0, sizeof(int) * nmemb);
+return (mol);
 }
