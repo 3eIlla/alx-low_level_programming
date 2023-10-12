5f8cfb9f568940f51edcf8b795183e2a6497fe63
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-10-12 15:00:37 -0700

    fix betty xf

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,14 +8,14 @@
 int(*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int lil = 0;
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int lil = 0;
 while (lil < 5)
 {
 	if (s && s[0] == ops[lil].op[0] && !s[1])
